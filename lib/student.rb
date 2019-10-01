77c7a303e1cc50b06f398bbbd19fdb165dd8b6c4
Marc Blanchard <marcblanchard0930@gmail.com>
2019-10-01 19:17:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,4 +62,9 @@ class Student
     sql = "SELECT * FROM students WHERE name = ?"
     DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
   end
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 end
