652bc207f00111d8b53b68cfa62d220657ad9b92
Marc Blanchard <marcblanchard0930@gmail.com>
2019-10-01 19:20:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,18 +45,18 @@ class Student
     end
   end
   
-  def self.create(name:, grade:)
-    student = Student.new(name, grade)
-    student.save
-    student
-  end
-  
   def self.new_from_db(row)
     id = row[0]
     name = row[1]
     grade = row[2]
     self.new(id, name, grade)
   end 
+  
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
   
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
